d8e40b11c66370c3dadb54bcfe2f8388cc44331d
guilhermediniz3 <gdiniztec@yahoo.com.br>
2025-02-01 16:47:17 -0300

    feat: color gradiente

--- terterqa/src/shared/components/navs/vertical/NavVertical.tsx
+++ terterqa/src/shared/components/navs/vertical/NavVertical.tsx
@@ -8,11 +8,11 @@ const NavVertical: React.FC = () => {
   const [selectedColor, setSelectedColor] = useState<string>('#333333'); // Cor inicial
 
   const toggleExpanded = () => {
-    setIsExpanded(prev => !prev);
+    setIsExpanded((prev) => !prev);
   };
 
   const toggleColorPicker = () => {
-    setIsColorPickerVisible(prev => !prev);
+    setIsColorPickerVisible((prev) => !prev);
   };
 
   const handleColorChange = (color: string) => {
@@ -21,7 +21,10 @@ const NavVertical: React.FC = () => {
   };
 
   return (
-    <nav className={`navVertical ${isExpanded ? 'expanded' : 'collapsed'}`} style={{ backgroundColor: selectedColor }}>
+    <nav
+      className={`navVertical ${isExpanded ? 'expanded' : 'collapsed'}`}
+      style={{ background: selectedColor }}
+    >
       {/* Botão de expansão */}
       <div className="verticalHeader">
         <button onClick={toggleExpanded} className="expandButton">
@@ -61,47 +64,43 @@ const NavVertical: React.FC = () => {
           <div className="colorOptions">
             <div
               className="colorOption"
-              style={{ backgroundColor: '#86168a' }}
-              onClick={() => handleColorChange('#86168a')}
+              style={{ backgroundColor: '#333333' }}
+              onClick={() => handleColorChange('#333333')}
             />
             <div
-              className="colorOption"
-              style={{ backgroundColor: '#ff6347' }}
-              onClick={() => handleColorChange('#ff6347')}
-            />      
-                          
-                       <div
-              className="colorOption"
-              style={{ backgroundColor: '#333333' }}
-              onClick={() => handleColorChange('#333333')}  
-            />
-                       <div
-              className="colorOption"
-              style={{ backgroundColor: '#4682B4' }}
-              onClick={() => handleColorChange('#4682B4')}
-            />
-                       <div
               className="colorOption"
               style={{ backgroundColor: '#FF0000' }}
               onClick={() => handleColorChange('#FF0000')}
             />
-          <div
+            <div
               className="colorOption"
               style={{ backgroundColor: '#32cd32' }}
               onClick={() => handleColorChange('#32cd32')}
             />
-                   <div
-              className="colorOption"
-              style={{ backgroundColor: '#708090' }}
-              onClick={() => handleColorChange('#708090')}
-            />
-            
             <div
               className="colorOption"
-              style={{ backgroundColor: '#' }}
-              onClick={() => handleColorChange('#FFD700')}
+              style={{ backgroundColor: '#2E5CE6' }}
+              onClick={() => handleColorChange('#2E5CE6')}
             />
-      
+            <div
+              className="colorOption"
+              style={{ backgroundColor: '#2ED0E6' }}
+              onClick={() => handleColorChange('#2ED0E6')}
+            />
+            <div
+              className="colorOption"
+              style={{ backgroundColor: '#812EE6' }}
+              onClick={() => handleColorChange('#812EE6')}
+            />
+            <div
+              className="colorOption"
+              style={{
+                background: 'linear-gradient(to right, #5A48F9, #312787)',
+              }}
+              onClick={() =>
+                handleColorChange('linear-gradient(to right, #5A48F9, #312787)')
+              }
+            />
             {/* Adicione mais opções de cores conforme necessário */}
           </div>
         )}
